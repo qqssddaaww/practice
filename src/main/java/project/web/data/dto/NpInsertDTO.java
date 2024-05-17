95c8dd6192ac47954e690b2ddd744f7956215d41
qqssddaaww <xkdlahxlr@naver.com>
2024-05-17 20:31:32 +0900

    05.17 version 01 charge 고정값 변경해야함

--- src/main/java/project/web/data/dto/NpInsertDTO.java
+++ src/main/java/project/web/data/dto/NpInsertDTO.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import project.web.data.domain.Hotel;
 import project.web.data.domain.Native;
 import project.web.data.domain.NativePage;
 import project.web.data.domain.Room;
@@ -17,33 +16,18 @@ import java.time.LocalDate;
 @NoArgsConstructor
 public class NpInsertDTO {
     private String content;
-    private String capacity;
-    private Long cost;
-    private String name;
     private String facility;
-    private String bed;
-    private String type;
-    private String no;
 
-    public NativePage nativePage(Hotel hotel, Native aNative) {
+    public NativePage nativePage(Native aNative, Room room) {
         return NativePage.builder()
                 .paCon(this.content)
                 .paFacility(this.facility)
                 .paDate(LocalDate.now())
                 .paRes(false)
-                .hotel(hotel)
                 .aNative(aNative)
-                .build();
-    }
-
-    public Room room() {
-        return Room.builder()
-                .rName(this.name)
-                .rBed(this.bed)
-                .rNo(this.no)
-                .rCost(this.cost)
-                .rCapacity(this.capacity)
-                .rType(this.type)
+                .room(room)
+//                일단 Charge 고정값넣어놈 변경
+                .paCharge(2)
                 .build();
     }
 
